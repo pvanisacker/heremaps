effb7062b5b2b567bc4d23edc1cf3a7c78d4512c
Pieter Van Isacker <pieter@localhost>
2014-10-09 15:55:56 +0200

    Clean up formatting

--- django/heremaps/views.py
+++ django/heremaps/views.py
@@ -5,19 +5,6 @@ from splunkdj.setup import create_setup_view_context
 from .forms import SetupForm
 from django.core.urlresolvers import reverse
 
-def get_app_id(request):
-    print("get_app_id")
-    service = request.service
-    app_id="none"
-    app_code="none"
-    for conf in service.confs:
-        if conf.name == "setup":
-            for stanza in conf.iter():
-                if stanza.name == "heremaps":
-                    app_id=stanza.content["app_id"]
-                    app_code=stanza.content["app_code"]
-    return {'app_id':app_id,'app_code':app_code}
-
 @render_to('heremaps:home.html')
 @login_required
 @config_required
