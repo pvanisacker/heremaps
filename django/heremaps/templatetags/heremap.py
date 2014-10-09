ae0deb696ad7b07bd06e31943dee46f754899d16
Pieter Van Isacker <pieter@localhost>
2014-10-09 16:35:39 +0200

    Add first cluster map version

--- django/heremaps/templatetags/heremap.py
+++ django/heremaps/templatetags/heremap.py
@@ -1,7 +1,12 @@
 from django import template
 from splunkdj.templatetags.tagutils import component_context
 register = template.Library()
+
 @register.inclusion_tag('splunkdj:components/component.html', takes_context=True)
 def heremarkermap(context, id, *args, **kwargs):
     return component_context(context,"heremarkermap",id,"view","heremaps/heremarkermap",kwargs)
+
+@register.inclusion_tag('splunkdj:components/component.html', takes_context=True)
+def hereclustermap(context, id, *args, **kwargs):
+    return component_context(context,"hereclustermap",id,"view","heremaps/hereclustermap",kwargs)
 
--- django/heremaps/urls.py
+++ django/heremaps/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
     url(r'^home/$', 'heremaps.views.home', name='home'),
     url(r'^heremarkermap_django/$', render('heremaps:heremarkermap_django.html'), name='heremarkermap_django'),
     url(r'^heremarkermap_js/$', render('heremaps:heremarkermap_js.html'), name='heremarkermap_js'),
+    url(r'^hereclustermap_django/$', render('heremaps:hereclustermap_django.html'), name='hereclustermap_django'),
+    url(r'^hereclustermap_js/$', render('heremaps:hereclustermap_js.html'), name='hereclustermap_js'),
     url(r'^setup/$', 'heremaps.views.setup', name='setup')
 )
