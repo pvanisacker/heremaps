359548f93ec4aaaacf75332aa1211c35481f391b
Pieter Van Isacker <van.isacker.pieter@gmail.com>
2015-01-22 22:27:12 +0100

    Try fixing test cases

--- test_unit_python.bash
+++ test_unit_python.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd bin/
-nosetests-2.7 -s --with-html-output --html-out-file=test-result.html --with-coverage3 --cover3-html --cover3-html-dir=coverage --cover3-package=tools tests/*
-cd ..
+nosetests-2.7 -s --with-html-output --html-out-file=test-result.html --with-coverage3 --cover3-html --cover3-html-dir=coverage --cover3-package=tools bin/tests/*
 
