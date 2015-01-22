6776b29e4c5c466c22b0bb186aa0f865073448f6
Pieter Van Isacker <van.isacker.pieter@gmail.com>
2015-01-22 23:58:49 +0100

    Add one more test

--- test/integration/search/reversegeocodeshape_command_test.py
+++ test/integration/search/reversegeocodeshape_command_test.py
@@ -15,3 +15,19 @@ class ReverseGeocodeShapeCommandTest(unittest.TestCase):
         self.assertEqual(result["lat"], "51")
         self.assertEqual(result["lng"], "3")
         self.assertEqual(result["key"], "BE")
+
+    def test_reverse_geocode_shape_command_custom_field(self):
+        search = "search * | head 1 | eval latitude=51 | eval longitude=3 | reversegeocodeshape lat=latitude lng=longitude | fields latitude,longitude,key"
+        result = return_first_result(self.splunkservice, search)
+        self.assertIsNotNone(result)
+        self.assertEqual(result["latitude"], "51")
+        self.assertEqual(result["longitude"], "3")
+        self.assertEqual(result["key"], "BE")
+
+    def test_reverse_geocode_shape_command_custom_map(self):
+        search = "search * | head 1 | eval lat=51 | eval lng=3 | reversegeocodeshape filetype=geojson filename=world2.geojson | fields lat,lng,key"
+        result = return_first_result(self.splunkservice, search)
+        self.assertIsNotNone(result)
+        self.assertEqual(result["lat"], "51")
+        self.assertEqual(result["lng"], "3")
+        self.assertEqual(result["key"], "BE")
