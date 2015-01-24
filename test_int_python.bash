23f0fb7fde2882bcde1b37c70e87766d0dd7d077
Pieter Van Isacker <pieter@localhost>
2015-01-24 16:15:43 +0100

    Improve integration tests

--- test_int_python.bash
+++ test_int_python.bash
@@ -5,7 +5,13 @@ rm -rf bin/lib/*.cache
 echo "----------------------------------------"
 echo "Running tests without cache"
 nosetests-2.7 -s --with-timer --with-html-output --html-out-file=python-int.html test/integration/search/*.py
+test_nocache=$?
 
 echo "----------------------------------------"
 echo "Running tests with cache"
 nosetests-2.7 -s --with-timer --with-html-output --html-out-file=python-int.html test/integration/search/*.py
+test_cache=$?
+
+if [ "$test_nocache" != 0 ] || [ "$test_cache" != 0 ] ; then
+    exit 1
+fi
