aea25d9c6be2672431a35cd5d4a082e9cafc384d
Pieter Van Isacker <pieter@localhost>
2015-01-24 16:14:26 +0100

    Improve code check

--- check_python.bash
+++ check_python.bash
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 pep8 --config=pep8 --exclude='*bin/splunklib*' bin/
+code=$?
 pep8 --config=pep8 test/integration/search
+test=$?
+if [ "$code" != 0 ] || [ "$test" != 0 ] ; then
+    exit 1
+fi
