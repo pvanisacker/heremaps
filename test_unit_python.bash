5fe3eff29d1b30b1137c610768dd8388c6c94697
Pieter Van Isacker <pieter@localhost>
2015-01-25 17:28:42 +0100

    Improve test run

--- test_unit_python.bash
+++ test_unit_python.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-nosetests-2.7 -s --with-html-output --html-out-file=python-unit.html --with-coverage3 --cover3-html --cover3-html-dir=coverage --cover3-package=tools bin/tests/*.py
+nosetests-2.7 -s --with-html-output --html-out-file=python-unit.html --with-coverage3 --cover3-html --cover3-html-dir=coverage --cover3-package=tools bin/tests/*_test.py
 
